a658a3dbe8acb20c250cd757427d3406888ded4f
trabnett <timrabnett@gmail.com>
2019-07-22 06:41:51 -0400

    login working

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -5,21 +5,46 @@ import { fetchToken } from '../../actions/signInActions';
 import { NavLink } from 'react-router-dom';
 
 import './SignIn.scss';
+import { ETIME } from 'constants';
 
 class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        username: "",
+        password: ""
+    }
+  }
+  handleChange = (e) => {
+    let updateState = {}
+    updateState[e.target.id.toLowerCase()] = e.target.value
+    this.setState(updateState)
+  }
+  submit = () => {
+    console.log(JSON.stringify(this.state))
+    fetch('http://178.128.233.31/frontend/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(this.state)
+    })
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(res => console.log("hello", res))
+
+  }
   render(){
     return (
       <div className="signin-container">
         <div >
             <form>
                 <div className="form-group">
-                    <input type="text" className="form-control" id="userName" placeholder="Username"></input>
+                    <input type="text" onChange={this.handleChange} className="form-control" id="userName" placeholder="Username"></input>
                 </div>
                 <div className="form-group">
-                    <input type="password" className="form-control" id="password" placeholder="Password"></input>
+                    <input type="password" onChange={this.handleChange} className="form-control" id="password" placeholder="Password"></input>
                 </div>
                 <div>
-                    <button type="submit" name="signIn" className="btn btn-info signin-btn">Login</button>
+                    <button type="button" onClick={this.submit} name="signIn" className="btn btn-info signin-btn">Login</button>
                 </div>
             </form>
         </div>
